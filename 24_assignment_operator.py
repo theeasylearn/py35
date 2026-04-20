0f9fdbcedd3dedd57b74f232d7b238b657fc9eab
theeasylearn <theeasylearn@gmail.com>
2026-04-20 10:33:31 +0530

    assignment operator

---
+++ 24_assignment_operator.py
@@ -0,0 +1,28 @@
+#example of assignment operators in python 
+a = 10 
+b = 3 
+
+# a = a + b 
+# or 
+a+=b
+print(f"after a+=b value of a= {a} b = {b}")
+
+a-=b
+print(f"after a-=b value of a= {a} b = {b}")
+
+a*=b
+print(f"after a*=b value of a= {a} b = {b}")
+
+a/=b
+print(f"after a/=b value of a= {a} b = {b}")
+
+a**=b
+print(f"after a/=b value of a= {a} b = {b}")
+
+a//=b
+print(f"after a/=b value of a= {a} b = {b}")
+
+a%=b
+print(f"after a/=b value of a= {a} b = {b}")
+
+

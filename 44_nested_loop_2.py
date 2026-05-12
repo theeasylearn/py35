a32a9400a165eccd8d82250aa62c96a8cf5fe050
theeasylearn <theeasylearn@gmail.com>
2026-05-12 09:55:04 +0530

    nested loop

---
+++ 44_nested_loop_2.py
@@ -0,0 +1,15 @@
+'''
+* * * * *
+* * * *
+* * *
+* *
+* 
+'''
+line = 5
+while line>=1:
+    astrik = 0
+    while astrik<line:
+        print("*",end=' ')
+        astrik=astrik + 1
+    print("") #new line 
+    line = line - 1
---
+++ 46_nested_loop_4.py
@@ -0,0 +1,19 @@
+'''
+        * 
+      * *
+    * * *
+  * * * *
+* * * * *
+'''
+row = 5
+count = 2
+while row>=1:
+  for space in range(1,row):
+    print(' ',end='')
+  for astrik in range(1,count):
+    print(" *",end='')
+  count=count+1
+  print("") #new line
+  row = row - 1
+
+

9c3d75afb2d561d7232387a00074f9603e6df0f3
theeasylearn <theeasylearn@gmail.com>
2026-07-11 09:32:23 +0530

    oop

---
+++ 77_oop_1.py
@@ -0,0 +1,19 @@
+#crete class 
+class MyMath:
+    #define methods(function)
+    def add(self):
+        print("I can do addition")
+    def sub(self):
+        print("I can do subtraction")
+    def mul(self):
+        print("I can do multiplication")
+    def div(self):
+        print("I can do division")
+    
+#we must create object to call methods of the MyMath class
+#object = ClassName()
+m1 = MyMath()
+m1.add()
+m1.sub()
+m1.mul()
+m1.div()
